0610ff948ad54180f998ebea6418d6529e6bf0fa
SUDHIR KUMAR <snghsudhirkumar06@gmail.com>
2025-01-24 17:03:26 +0530

    dynamic details

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from .models import Category,Photo
 # Create your views here.
 def gallary(request):
-    return render(request,'photos/gallary.html')
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories':categories , 'photos':photos}
+    return render(request,'photos/gallary.html',context)
 
 def viewPhoto(request,pk):
     return render(request,'photos/photo.html')
