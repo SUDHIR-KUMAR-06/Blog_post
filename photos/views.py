604c16ee0bf2154d7c897d123df7f2fe8e20e378
SUDHIR KUMAR <snghsudhirkumar06@gmail.com>
2025-02-10 23:35:46 +0530

    push

--- photos/views.py
+++ photos/views.py
@@ -1,39 +1,14 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from .models import Category,Photo
 # Create your views here.
 def gallary(request):
-    category=request.GET.get('category')
-    if category==None:
-        photos = Photo.objects.all()
-    else:
-        photos = Photo.objects.filter(category__name=category)
-
     categories = Category.objects.all()
-   
+    photos = Photo.objects.all()
     context = {'categories':categories , 'photos':photos}
     return render(request,'photos/gallary.html',context)
 
 def viewPhoto(request,pk):
-    photo=Photo.objects.get(id=pk)
-    return render(request,'photos/photo.html',{'photo':photo})
+    return render(request,'photos/photo.html')
 
 def addPhoto(request):
-    categories = Category.objects.all()
-    if request.method == 'POST':
-        data=request.POST
-        image=request.FILES.get('image')
-
-        if data['category']!='none':
-            category=Category.objects.get(id=data['category'])
-        elif data['category_new']!='':
-            category,created=Category.objects.get_or_create(name=data['category_new'])
-        else:
-            category=None
-        photo=Photo.objects.create(
-            category=category,
-            description=data['description'],
-            image=image,
-        )
-        return redirect('gallary')
-    context = {'categories':categories }
-    return render(request,'photos/add.html',context)
+    return render(request,'photos/add.html')
